ed92eb881b6654af44e6faf8c29ab95705db9a73
aroldoa <aroldo@primomotif.com>
2016-01-29 17:45:33 -0600

    Added Filename custom theme

---
+++ wp-content/themes/file-time/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package FileTime
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'file-time' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'file-time' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'file-time' ), 'file-time', '<a href="http://www.primomotif.com" rel="designer">Primo Motif Ad & Design Agency</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
